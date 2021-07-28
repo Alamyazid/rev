086147ee082316b6ed168e1b2fdbd3916488188f
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-07-28 22:02:30 +0800

    Update ssrmu.sh

--- ssrmu.sh
+++ ssrmu.sh
@@ -3,16 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-echo "Checking VPS"
-IZIN=$( curl http://akses.rpj08.my.id:81/akses | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
-clear
+
 #PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 sh_ver="1.0.26"
