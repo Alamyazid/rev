5ac0184d058dc9497e2c6615ee30b5653d9f5b73
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-07-29 22:01:43 +0800

    Update upgrade-menu.sh

--- upgrade-menu.sh
+++ upgrade-menu.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-#Upgrade-menu
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ifconfig.me/ip);
 
+echo "Start Update"
+# update
+cd /usr/bin
 wget -O /usr/bin/l2tp https://raw.githubusercontent.com/Alamyazid/rev/main/update/l2tp.sh && chmod +x /usr/bin/l2tp
 wget -O /usr/bin/menu https://raw.githubusercontent.com/Alamyazid/rev/main/update/menu.sh && chmod +x /usr/bin/menu
 wget -O /usr/bin/ssh https://raw.githubusercontent.com/Alamyazid/rev/main/update/ssh.sh && chmod +x /usr/bin/ssh
