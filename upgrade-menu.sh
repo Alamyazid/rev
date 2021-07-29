699fecdf66a522d23fc631d100274590bd325ab9
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-07-29 18:44:05 +0800

    Update upgrade-menu.sh

--- upgrade-menu.sh
+++ upgrade-menu.sh
@@ -1,5 +1,5 @@
 
-cd /usr/bin/
+cd /usr/bin
 wget -O /usr/bin/ l2tp https://raw.githubusercontent.com/Alamyazid/rev/main/update/l2tp.sh && chmod +x /usr/bin/l2tp
 wget -O /usr/bin/menu https://raw.githubusercontent.com/Alamyazid/rev/main/update/menu.sh && chmod +x /usr/bin/menu
 wget -O /usr/bin/ssh https://raw.githubusercontent.com/Alamyazid/rev/main/update/ssh.sh && chmod +x /usr/bin/ssh
@@ -9,3 +9,15 @@ wget -O /usr/bin/trojaan https://raw.githubusercontent.com/Alamyazid/rev/main/up
 wget -O /usr/bin/v2raay https://raw.githubusercontent.com/Alamyazid/rev/main/update/v2raay.sh && chmod +x /usr/bin/v2raay
 wget -O /usr/bin/vleess https://raw.githubusercontent.com/Alamyazid/rev/main/update/vleess.sh && chmod +x /usr/bin/vleess
 wget -O /usr/bin/wgr https://raw.githubusercontent.com/Alamyazid/rev/main/update/wgr.sh && chmod +x /usr/bin/wgr
+
+
+cd /usr/bin
+chmod +x /usr/bin/l2tp
+chmod +x /usr/bin/menu
+chmod +x /usr/bin/ssh
+chmod +x /usr/bin/ssssr
+chmod +x /usr/bin/sstpp
+chmod +x /usr/bin/trojaan
+chmod +x /usr/bin/v2raay
+chmod +x /usr/bin/vleess
+chmod +x /usr/bin/wgr
