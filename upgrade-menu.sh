775a6e3002e3fe3daa249f393b5738c1c92b513a
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-07-29 17:55:10 +0800

    Add files via upload

---
+++ upgrade-menu.sh
@@ -0,0 +1,29 @@
+
+cd /usr/bin
+rm -r menu
+
+cd /usr/bin
+wget -O l2tp "https://raw.githubusercontent.com/Alamyazid/rev/main/update/l2tp.sh"
+wget -O menu "https://raw.githubusercontent.com/Alamyazid/rev/main/update/menu.sh"
+wget -O ssh "https://raw.githubusercontent.com/Alamyazid/rev/main/update/ssh.sh"
+wget -O ssssr "https://raw.githubusercontent.com/Alamyazid/rev/main/update/ssssr.sh"
+wget -O sstpp "https://raw.githubusercontent.com/Alamyazid/rev/main/update/sstpp.sh"
+wget -O trojaan "https://raw.githubusercontent.com/Alamyazid/rev/main/update/trojaan.sh"
+wget -O v2raay "https://raw.githubusercontent.com/Alamyazid/rev/main/update/v2raay.sh"
+wget -O vleess "https://raw.githubusercontent.com/Alamyazid/rev/main/update/vleess.sh"
+wget -O wgr "https://raw.githubusercontent.com/Alamyazid/rev/main/update/wgr.sh"
+
+chmod +x l2tp
+chmod +x menu
+chmod +x ssh
+chmod +x ssssr
+chmod +x sstpp
+chmod +x trojaan
+chmod +x v2raay
+chmod +x vleess
+chmod +x wgr
+
+clear
+echo " Reboot 10 Sec"
+sleep 10
+reboot
