53f5b95041be6b2954864dbb97201de7d2959860
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-07-30 20:39:26 +0800

    Update set-br.sh

--- set-br.sh
+++ set-br.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curl https://rclone.org/install.sh | bash
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/lesta-1/sc/main/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/Alamyazid/rev/main/rclone.conf"
 git clone  https://github.com/magnific0/wondershaper.git
 cd wondershaper
 make install
@@ -19,19 +19,19 @@ account default
 host smtp.gmail.com
 port 587
 auth on
-user pradiasangindraswari@gmail.com
-from pradiasangindraswari@gmail.com
-password ybkehwuifdgvgiiq
+user exeldept13@gmail.com
+from exeldept13@gmail.com
+password Alamyazid01
 logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/lesta-1/sc/main/autobackup.sh"
-wget -O backup "https://raw.githubusercontent.com/lesta-1/sc/main/backup.sh"
-wget -O bckp "https://raw.githubusercontent.com/lesta-1/sc/main/bckp.sh"
-wget -O restore "https://raw.githubusercontent.com/lesta-1/sc/main/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/lesta-1/sc/main/strt.sh"
-wget -O limit-speed "https://raw.githubusercontent.com/lesta-1/sc/main/limit-speed.sh"
+wget -O autobackup "https://raw.githubusercontent.com/Alamyazid/rev/main/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/Alamyazid/rev/main/backup.sh"
+wget -O bckp "https://raw.githubusercontent.com/Alamyazid/rev/main/bckp.sh"
+wget -O restore "https://raw.githubusercontent.com/Alamyazid/rev/main/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/Alamyazid/rev/main/strt.sh"
+wget -O limit-speed "https://raw.githubusercontent.com/Alamyazid/rev/main/limit-speed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x bckp
