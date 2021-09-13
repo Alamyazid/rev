6b9e26f675b42e4ddbabccfdd56234ff5a770a3c
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-09-14 01:03:12 +0800

    Update proxy-templatedssl.py

--- proxy-templatedssl.py
+++ proxy-templatedssl.py
@@ -6,14 +6,14 @@ LISTENING_ADDR = '0.0.0.0'
 if sys.argv[1:]:
   LISTENING_PORT = sys.argv[1]
 else:
-  LISTENING_PORT = 443
+  LISTENING_PORT = 700
 # Pass
 PASS = ''
 
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:143'
+DEFAULT_HOST = '127.0.0.1:69'
 RESPONSE = 'HTTP/1.1 101 Switching Protocols\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
