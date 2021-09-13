95621c9ea5f28e3e0201b22fdd532f49b839a11b
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-09-13 23:57:30 +0800

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -59,7 +59,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy 8880
+ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxy
 Restart=on-failure
 
 [Install]
@@ -73,32 +73,32 @@ systemctl restart edu-proxy
 clear
 
 # Getting Proxy Template Ssl
-wget -q -O /usr/local/bin/edu-proxyssl https://raw.githubusercontent.com/Alamyazid/rev/main/proxy-templatedssl.py
-chmod +x /usr/local/bin/edu-proxyssl
+#wget -q -O /usr/local/bin/edu-proxyssl https://raw.githubusercontent.com/Alamyazid/rev/main/proxy-templatedssl.py
+#chmod +x /usr/local/bin/edu-proxyssl
 
 # Installing Service
-cat > /etc/systemd/system/edu-proxyssl.service << END
-[Unit]
-Description=Python Edu Ssl Proxy By Radenpancal Service
-Documentation=https://lestakun.tech
-After=network.target nss-lookup.target
+#cat > /etc/systemd/system/edu-proxyssl.service << END
+#[Unit]
+#Description=Python Edu Ssl Proxy By Radenpancal Service
+#Documentation=https://lestakun.tech
+#After=network.target nss-lookup.target
 
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxyssl
-Restart=on-failure
+$[Service]
+#Type=simple
+#User=root
+#CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#NoNewPrivileges=true
+#ExecStart=/usr/bin/python -O /usr/local/bin/edu-proxyssl
+#Restart=on-failure
 
-[Install]
-WantedBy=multi-user.target
-END
+#[Install]
+#WantedBy=multi-user.target
+#END
 
-systemctl daemon-reload
-systemctl enable edu-proxyssl
-systemctl restart edu-proxyssl
+#systemctl daemon-reload
+#systemctl enable edu-proxyssl
+#systemctl restart edu-proxyssl
 
 clear
 
