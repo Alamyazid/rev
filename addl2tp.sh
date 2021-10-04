0e9488aa1b59684e803aad37f38670e9fb9107ce
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-10-04 19:37:13 +0800

    Update addl2tp.sh

--- addl2tp.sh
+++ addl2tp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source /var/lib/premium-script/ipvps.conf
 if [[ "$IP" = "" ]]; then
-PUBLIC_IP=$(wget -qO- ipv4.icanhazip.com);
+domain=$(cat /etc/v2ray/domain)
 else
-PUBLIC_IP=$IP
+domain=$IP
 fi
 until [[ $VPN_USER =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		read -rp "Username: " -e VPN_USER
