7507aeb4632f21674aaba96f1248989dbad7cebe
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-10-12 01:29:30 +0800

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 read -rp "Masukan domain anda, atau enter untuk domain otomatis.."
-read -rp "Domain/Host: "
+read -rp "Domain/Host: $subdomain "
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
+echo $subdomain > etc/v2ray/domain
