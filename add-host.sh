3b684c6c6902078bddd13e016e0b234227288d7d
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-10-12 01:26:12 +0800

    Update add-host.sh

--- add-host.sh
+++ add-host.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-read -e -p " Masukan domain anda, atau enter untuk domain otomatis..
-read -rp "Domain/Host: " -e host
+read -rp "Masukan domain anda, atau enter untuk domain otomatis.."
+read -rp "Domain/Host: "
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
