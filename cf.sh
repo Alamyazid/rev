54f0bdc0e4eaac2c59ff9023592294f216e72fff
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-10-11 23:27:43 +0800

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -7,7 +7,7 @@ MYIP=$(wget -qO- ifconfig.me/ip);
 apt install jq curl -y
 DOMAIN=iwaksapat.my.id
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
-SUB_DOMAIN=${sub}.iwaksapat.my.id
+SUB_DOMAIN=${sub}.kalimantan-rev.my.id
 CF_ID=alamyazid13@gmail.com
 CF_KEY=5819ee35d91ccb227cc14fcb057c1fb02025c
 set -euo pipefail
