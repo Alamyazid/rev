8261ab0871b4c5d86e98aa9007e6a80a188af0cc
Tuan BabaL <48976654+Alamyazid@users.noreply.github.com>
2021-10-15 19:36:49 +0800

    Update set-br.sh

--- set-br.sh
+++ set-br.sh
@@ -19,9 +19,9 @@ account default
 host smtp.gmail.com
 port 587
 auth on
-user exeldept13@gmail.com
-from exeldept13@gmail.com
-password egtcdxpovpucnxod
+user tuanyazid13@gmail.com
+from tuanyazid13@gmail.com
+password aeaoyjascvppuygu
 logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
