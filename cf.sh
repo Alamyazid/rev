181e03724d4732ee0566fefa44d85b0747639393
TuanYZ <48976654+Alamyazid@users.noreply.github.com>
2022-01-18 13:50:47 +0800

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -5,7 +5,7 @@ NC='\e[0m'
 MYIP=$(wget -qO- ifconfig.me/ip);
 
 apt install jq curl -y
-DOMAIN=iwaksapat.my.id
+DOMAIN=iwaksapat.site
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
 SUB_DOMAIN=${sub}.iwaksapat.site
 CF_ID=exeldept13@gmail.com
@@ -40,8 +40,8 @@ echo "Host : $SUB_DOMAIN"
 echo $SUB_DOMAIN > /root/domain
 #mail.conf
 cat > /root/mail.conf << END
-DOMAIN=iwaksapat.my.id
-CF_ID=alamyazid13@gmail.com
-CF_KEY=5819ee35d91ccb227cc14fcb057c1fb02025c
+DOMAIN=iwaksapat.site
+CF_ID=exeldept13@gmail.com
+CF_KEY=cd9375e00bd60eef46d1bdde69337d468ce2b
 END
 rm -f /root/cf.sh
